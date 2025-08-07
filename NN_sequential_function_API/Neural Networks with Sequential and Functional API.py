ce982f848941b2f981cfd8ee4b06208b729751ac
Himanshu Gangwar <151658698+zenitsu0509@users.noreply.github.com>
2025-08-07 19:28:06 +0530

    Update Neural Networks with Sequential and Functional API.py

--- NN_sequential_function_API/Neural Networks with Sequential and Functional API.py
+++ NN_sequential_function_API/Neural Networks with Sequential and Functional API.py
@@ -5,6 +5,7 @@ from tensorflow.keras import layers
 from tensorflow.keras.datasets import mnist
 import matplotlib.pyplot as plt
 
+
 (x_train,y_train),(x_test,y_test) = mnist.load_data()
 x_train = x_train.reshape(-1,28*28).astype("float32")/255.0
 x_test = x_test.reshape(-1,28*28).astype("float32")/255.0
